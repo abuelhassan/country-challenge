f13d02e73878371a2e3248e276843c495e09efae
amr <abuelhassan93@gmail.com>
2026-04-12 16:21:23 +0100

    separeate carribbean into sublist

--- scripts/list_countries_csv.py
+++ scripts/list_countries_csv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Read all country GeoJSON files from countries/ and write a CSV with id, game_name, and alts.
+Read all country GeoJSON files from countries/ and write a CSV with id, continent, game_name, and alts.
+Sorted by continent then id.
 
 Usage:
     python scripts/list_countries_csv.py [--out countries_list.csv]
@@ -24,7 +25,7 @@ for path in sorted(Path('countries').glob('*.geo.json')):
         rows.append({
             'id': feature.get('id', path.name.replace('.geo.json', '')),
             'game_name': feature['properties'].get('game_name', ''),
-            'continent': feature['properties'].get('continent', ''),
+            'continent': feature['properties'].get('continent', 'Other'),
             'alts': '|'.join(alts),
         })
 
